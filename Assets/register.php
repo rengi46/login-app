697feac862a1e3a9282047785acd2f6ced4f5b6d
Erick Ayala <erickayalrias@gmail.com>
2021-12-15 19:22:27 +0100

    login json

--- Assets/register.php
+++ Assets/register.php
@@ -2,13 +2,14 @@
 
 if(isset($_POST)){
     $arr_clients= array("name" =>$_POST["name"],"email"=> $_POST["email"], "password" => $_POST["password"] );
-    $json_string = json_encode($arr_clients);
+    // $json_string = json_encode($arr_clients);
     $file="./json/users.json";
-    file_put_contents($file, $json_string);
-}
+    $Allusers= file_get_contents($file);
+    $usersAll= json_decode($Allusers);
+    // print_r($Allusers);
+    print_r($usersAll);
+    array_push($usersAll , $arr_clients);
+    $jsonUsers= json_encode($usersAll);
+    file_put_contents($file, $jsonUsers);
 
-// $json_string= json_encode($arr_clients);
-// $file="./json/users.json";
-// file_put_contents($file,$json_string);
-// }
-// header("location: ");
+}
